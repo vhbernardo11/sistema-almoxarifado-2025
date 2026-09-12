45854bfed41029cd15488b59e343e0fe967d8781
vhbernardo11 <vhbernardo11@gmail.com>
2026-09-11 23:44:10 -0300

    feat: export stage13 runtime helpers

--- src/integra/runtime/__init__.py
+++ src/integra/runtime/__init__.py
@@ -2,6 +2,13 @@ from .guards import Stage8TestModeViolation, TestActor, require_stage8_test_acto
 from .handlers import build_stage8_handlers
 from .launcher import build_stage8_worker
 from .stage10 import build_stage10_handlers, build_stage10_worker
+from .stage13 import (
+    JOB_TYPE_TO_SPECIALIST,
+    SPECIALIST_JOB_TYPES,
+    build_specialist_job_payload,
+    enqueue_specialist_task,
+    execute_specialist_job,
+)
 
 __all__ = [
     "Stage8TestModeViolation",
@@ -11,4 +18,9 @@ __all__ = [
     "build_stage8_worker",
     "build_stage10_handlers",
     "build_stage10_worker",
+    "SPECIALIST_JOB_TYPES",
+    "JOB_TYPE_TO_SPECIALIST",
+    "build_specialist_job_payload",
+    "enqueue_specialist_task",
+    "execute_specialist_job",
 ]
